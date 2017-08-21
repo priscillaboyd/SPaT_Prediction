bdfa368b56bd1e14384b3137134a57627447f98e
priscillaboyd <priscillaboyd@users.noreply.github.com>
2017-08-21 15:51:15 +0100

    Added ability to create sklearn dataset to DataPreProcessing, tidy up of CART (Decision Tree) algorithm implementation

--- src/DT.py
+++ src/DT.py
@@ -24,115 +24,55 @@ Implements the Random Forest ensemble
 """
 import pandas as pd
 
-# declare data path
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor
-
-data = '../results/20170813_114136/dataset.csv'
-
-# select fields that we want to use for DT
-fields = ['Date', 'Time', 'Result', 'Phase']
-
-# load data with selected fields and parse date/time to a single Date_Time column
-phase_data = pd.read_csv(data, header=0, skipinitialspace=True, usecols=fields, parse_dates=[['Date', 'Time']])
-df = pd.DataFrame(phase_data)
-
-# load list of phases and states
-phase_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
+import matplotlib.pyplot as plt
 
 
-# prepare data for scikit-learn decision tree processing
-def prepare_dt_data():
-
-    new_columns = ['Phase', 'Result', 'Start', 'End', 'Duration']
-    df_new_columns = pd.DataFrame(columns=new_columns)
-
-    # write file for the first time with header
-    df_new_columns.to_csv('../results/20170813_114136/scikit_dataset.csv', sep=',', index=False, mode='w')
-
-    # loop through phases
-    for x in range(len(phase_list)):
-        phase = phase_list[x]
-
-        # create a df for phase A only
-        df2 = df[df['Phase'] == phase]
-
-        print("Preparing data...")
-        # initialise by using the very first record
-        start_time = df2['Date_Time'].values[0]
-        current_result = df2['Result'].values[0]
-
-        # loop through all records from DF to process duration
-        for i in range(len(df2.index)):
-
-            # if the phase is the same, set as end time
-            if df2['Result'].values[i] == current_result:
-                end_time = df2['Date_Time'].values[i]
-
-            # if the phase is no longer the same or it's the last record, use end time so far to get duration
-            if df2['Result'].values[i] != current_result or i+1 == len(df2.index):
-                df_start = pd.to_datetime(start_time)
-                df_end = pd.to_datetime(end_time)
-                duration = pd.Timedelta(df_end - df_start).seconds
-
-                # if the time is the same, force duration = 1
-                if duration == 86399.0:
-                    df_end = df_start
-                    duration = 1.0
-
-                # convert phase ID to int (to cater for scikit-learn requirements)
-                phase_value = str(x)
-
-                # write new row to data frame
-                new_row = [phase_value, current_result, df_start, df_end, duration]
-                df_new_columns.loc[(len(df_new_columns))] = new_row
-
-                # go to the next result and start time
-                current_result = df2['Result'].values[i]
-                start_time = df2['Date_Time'].values[i]
-
-        print(df_new_columns)
-
-    # write result further to csv
-    df_new_columns.to_csv('../results/20170813_114136/scikit_dataset.csv', sep=',', index=False,
-                              header=False, mode='a')
-    print("Prepared dataset available: ../results/20170813_114136/scikit_dataset.csv")
-    return df_new_columns
+# get X and y for sklearn models, excluding date/time stamps
+def get_sklearn_data():
+    data = pd.read_csv('../results/20170821_154732/scikit_dataset.csv', usecols=['Phase', 'Result', 'Duration'],
+                       sep=',')
+    X = data.drop('Duration', axis=1)
+    y = data.Duration
+    return X, y
 
 
-# implement the Random Forest ensemble algorithm
-def run_random_forest():
-    # Get dataset
-    df_train = pd.read_csv('../results/20170813_114136/scikit_dataset.csv')
+# implement CART
+def run_CART():
+    # declare X and y
+    X, y = get_sklearn_data()
 
-    cols = ['Phase', 'Result']
-    X = df_train[cols]
-    y = df_train['Duration']
+    # split data into training / test (20% for test)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-    # split data into train / test (20% for test)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    rf_model = RandomForestRegressor(n_estimators=1000, min_samples_split=6, oob_score=True, min_samples_leaf=5,
-                                     criterion='mae')
+    # create decision tree model
+    dt_model = DecisionTreeRegressor(max_depth=4)
+    # rf_model = RandomForestRegressor(n_estimators=1000, oob_score=True, random_state=1)
 
     # fit model using training data
-    rf_model.fit(X_train, y_train)
+    dt_model.fit(X_train, y_train)
+    dt_model.predict(X_test)
+
+    # expose to tree graphviz format for analysis
+    export_graphviz(dt_model, out_file='../results/20170813_114136/dt_tree.dot', feature_names=X_train.columns)
 
     # predict on new (test) data and encapsulate result in data frame
-    y_predicted = rf_model.predict(X_test)
-    result = pd.DataFrame(y_predicted)
+    y_prediction = dt_model.predict(X_test)
 
     # get the coefficient of determination to measure how good the random forest model is
-    score = rf_model.score(X_test, y_test)
-
-    print(result)
-    print("Total number of features:", rf_model.n_features_)
+    score = dt_model.score(X_test, y_test)
     print("Total score:", score)
 
-    # pd.DataFrame.to_csv(df_result,'../results/20170813_114136/result.csv')
+    # plot for visualisation
+    plt.scatter(y_prediction, y_test, label='Duration')
+    plt.plot([0, 1], [0, 1], '--k', transform=plt.gca().transAxes)
+    plt.xlabel('y_prediction')
+    plt.ylabel('y_test')
+    plt.show()
 
 # main function runs data processing for decision trees
 if __name__ == '__main__':
-    df = prepare_dt_data()
-    run_random_forest()
+    # df = prepare_dt_data()
+    # run_random_forest()
+    run_CART()
--- src/DataPreProcessing.py
+++ src/DataPreProcessing.py
@@ -36,7 +36,6 @@ Combines the data by:
 import pandas as pd
 from pathlib import Path
 import os
-
 import time
 
 raw_data = './data/sample_data_small.csv'
@@ -49,7 +48,7 @@ source_data = source_data[~source_data['Mode Stream 0'].isin(['8 - SUP '])]
 current_dt = time.strftime("%Y%m%d_%H%M%S")
 
 # create folder for results
-results_folder = '../results/' + current_dt + '/'
+results_folder = './results/' + current_dt + '/'
 if not os.path.exists(results_folder):
     os.makedirs(results_folder, mode=0o777)
 
@@ -260,11 +259,82 @@ def data_merge():
     merged_data.to_csv(results_folder + 'dataset.csv', sep=',', index=False)
     print("Data merged!")
     print("Final dataset available: " + results_folder + 'dataset.csv')
+    return results_folder + 'dataset.csv'
+
+
+def sklearn_data_processing(dataset):
+    # select fields that we want to use for data frame
+    fields = ['Date', 'Time', 'Result', 'Phase']
+
+    # load data and parse date/time to a single Date_Time column
+    phase_data = pd.read_csv(dataset, header=0, skipinitialspace=True, usecols=fields, parse_dates=[['Date', 'Time']])
+    df = pd.DataFrame(phase_data)
+
+    # load list of phases and states
+    phase_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+
+    new_columns = ['Phase', 'Result', 'Start', 'End', 'Duration']
+    df_new_columns = pd.DataFrame(columns=new_columns)
+
+    # write file for the first time with header
+    df_new_columns.to_csv(results_folder + 'sklearn_dataset.csv', sep=',', index=False, mode='w')
+
+    # loop through phases
+    for x in range(len(phase_list)):
+        phase = phase_list[x]
+
+        # create a df for phase A only
+        df2 = df[df['Phase'] == phase]
+
+        print("Preparing data...")
+        # initialise by using the very first record
+        start_time = df2['Date_Time'].values[0]
+        current_result = df2['Result'].values[0]
+
+        # loop through all records from DF to process duration
+        for i in range(len(df2.index)):
+
+            # if the phase is the same, set as end time
+            if df2['Result'].values[i] == current_result:
+                end_time = df2['Date_Time'].values[i]
+
+            # if the phase is no longer the same or it's the last record, use end time so far to get duration
+            if df2['Result'].values[i] != current_result or i+1 == len(df2.index):
+                df_start = pd.to_datetime(start_time)
+                df_end = pd.to_datetime(end_time)
+                duration = pd.Timedelta(df_end - df_start).seconds
+
+                # if the time is the same, force duration = 1
+                if duration == 86399.0:
+                    df_end = df_start
+                    duration = 1.0
+
+                # convert phase ID to int (to cater for scikit-learn requirements)
+                phase_value = str(x)
+
+                # write new row to data frame
+                new_row = [phase_value, current_result, df_start, df_end, duration]
+                df_new_columns.loc[(len(df_new_columns))] = new_row
+
+                # go to the next result and start time
+                current_result = df2['Result'].values[i]
+                start_time = df2['Date_Time'].values[i]
+
+        print(df_new_columns)
+
+    # write result further to csv
+    df_new_columns.to_csv(results_folder + 'sklearn_dataset.csv', sep=',', index=False,
+                          header=False, mode='a')
+    print("Prepared dataset available: " + results_folder + "sklearn_dataset.csv")
 
 
 # main function runs extract, clean and merge data processes
 if __name__ == '__main__':
+    # extract, clean and merge data
     data_extract()
     data_cleaning()
-    data_merge()
+    merged_data = data_merge()
+
+    # process data to use with scikit-learn models
+    sklearn_data_processing(merged_data)
 
